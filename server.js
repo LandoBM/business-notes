0c27108e568105f92773e47ad865fe0ab631633c
Landon Murray <landonbmurray@icloud.com>
2023-01-20 01:53:43 -0500

    updated repo name

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.get('/notes', (req, res) => {
 
 app.get('/api/notes', (req,res) => {
     fs.readFile('./db/db.json', 'utf8', (err,data) => {
-      if (err) {
-        console.log(err)
-      } else {
-        res.json(data)
-      }
+    //   if (err) {
+    //     console.log(err)
+    //   } else {
+    //     res.json(data)
+    //   }
     })
     console.info(`${req.method} request received to get notes html file`)
 })

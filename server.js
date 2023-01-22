7c7c6280199f254087701f8b7329f9fb24f9b893
Landon Murray <landonbmurray@icloud.com>
2023-01-22 01:17:22 -0500

    working on POST, getting an error when trying to post

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
+// UUID method for generating unique ids
+const { v4: uuidv4 } = require('uuid');
+// use uuidv4() for unique ID
+
 const dbData = require('./db/db.json')
-// Using any PORT available in case 3001 is not available
-const PORT = process.env.PORT || 3001
+// Using any PORT available in case 5500 is not available
+const PORT = process.env.PORT || 5500
 
 // Setting up the Express app to handle parsing
 app.use(express.urlencoded({extended:true}))
@@ -19,13 +23,13 @@ app.get('/', (req, res) => {
 })     
 
 
-
+// Serve the Notes File
 app.get('/api/notes', (req,res) => {
     fs.readFile('./db/db.json', 'utf8', (err,data) => {
       if (err) {
         console.log(err)
       } else {
-        // Added Const to make the data parsed when shown. 
+        // Added const to make the data parsed when shown. 
         const dataParse = JSON.parse(data)
         res.json(dataParse)
       }
@@ -36,6 +40,45 @@ app.get('/api/notes', (req,res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/index.html`))
     console.log(`${req.method} request received to get index html file`)
+})
+
+
+app.post('/api/notes', (req, res) => {
+// Logging that POST request was received
+ console.info(`${req.method} request received to add a note`)
+ 
+//  Deconstructing assignment for the items in req.body
+ const {title, text} = req.body
+// If the required properties are available (title, text)
+ if (title && text) {
+    // Created new variable for the object that will save
+    const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+    }
+
+    fs.readFile(`${dbData}`, 'utf8').then(function(data) {
+        // const note = req.body
+        const dataParse = JSON.parse(data)
+        note.id = dataParse.length ++
+        dataParse.push(newNote)
+        return dataParse
+    }).then(function(dataParse) {
+        fs.writeFile(`${dbData}`, JSON.stringify(dataParse, null))
+        res.json(newNote)
+    })
+
+    const response = {
+        status: 'success',
+        body: newNote,
+    }
+
+    console.log(response)
+    res.status(201).json(response)
+ } else {
+    res.status(500).json(`Error in posting note`)
+ }
 })
 
 app.listen(PORT, () => {
